eb6b26a5a078431fd76a81796337ad9c67e7243e
pedrocx486 <pedrocx486@gmail.com>
2022-07-09 04:43:02 -0300

    Jesus Christ.

--- webapp/src/app/monitor/monitorscreen.component.ts
+++ webapp/src/app/monitor/monitorscreen.component.ts
@@ -22,8 +22,7 @@ export class MonitorScreenComponent implements OnInit {
   // 2 = Print Progress
   // 3 = Nozzle Temperature
 
-  connectionStatus: any;
-  printerData = [{ status: '' }];
+  connectionStatus: string;
   printerStatusList = ['MachineStatus: READY', 'MachineStatus: BUILDING_FROM_SD'];
   printerStatus = '';
   printProgress = 0;
@@ -39,29 +38,36 @@ export class MonitorScreenComponent implements OnInit {
 
   getPrinterData(): void {
     this.connectionStatus = this.connectionStatus ? 'Fetching...' : 'Connecting...';
-    this.fetchData(this.baseURL + '/flashforge/requestData').toPromise().then((data: any) => {
-      this.printerData = data;
-      this.parseData();
-    }).catch(() => {
-      this.printerData[0].status = '';
-      this.parseData();
+    this.fetchData(this.baseURL + '/flashforge/requestData').subscribe({
+      next: (data: any) => {
+        this.parseData(data);
+      },
+      error: () => {
+        this.parseData({ status: '' });
+      }
     });
-  }
-
-  parseData(): void {
-    if (this.printerData[0].status) {
-      this.connectionStatus = this.printerData[0].status.includes('Success') ? 'Connected' : 'Unknown connection error.';
-      this.printerStatus = this.printerData[1].status.includes(this.printerStatusList[0]) ?
-        'Idle/Ready' : this.printerData[1].status.includes(this.printerStatusList[1]) ? 'Printing' : 'Unknown Status';
-      this.printProgress = this.printerData[2].status ? Number(this.printerData[2].status.split('byte ')[1].split('/')[0]) : 0;
-      this.nozzleTemperature = this.printerData[3].status ?
-        this.printerData[3].status.split(' ')[1].replace('T0:', '') + 'ºC' : 'Unknown Temperature';
-    } else {
-      this.connectionStatus = 'Printer offline. (No Response)';
-    }
   }
 
   fetchData(url: string): Observable<any> {
     return this.http.get(url);
+  }
+
+  parseData(printerData: any): void {
+    // If any rest api returned this to me today, i'd go after the dev and steal his left shoe.
+    // TODO: Refactor the API.
+
+    if (printerData[0].status) {
+      this.connectionStatus = printerData[0].status.includes('Success') ? 'Connected' : 'Unknown connection error.';
+
+      this.printerStatus = printerData[1].status.includes(this.printerStatusList[0]) ?
+        'Idle/Ready' : printerData[1].status.includes(this.printerStatusList[1]) ? 'Printing' : 'Unknown Status';
+
+      this.printProgress = printerData[2].status ? Number(printerData[2].status.split('byte ')[1].split('/')[0]) : 0;
+
+      this.nozzleTemperature = printerData[3].status ?
+        printerData[3].status.split(' ')[1].replace('T0:', '') + 'ºC' : 'Unknown Temperature';
+    } else {
+      this.connectionStatus = 'Printer offline. (No Response)';
+    }
   }
 }
