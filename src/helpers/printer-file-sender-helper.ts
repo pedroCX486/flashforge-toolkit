6ecca2c1194699663a5d5759c2640e76a4f6129d
pedrocx486 <pedrocx486@gmail.com>
2023-05-28 07:08:14 -0300

    Still getting crc errors...

--- src/helpers/printer-file-sender-helper.ts
+++ src/helpers/printer-file-sender-helper.ts
@@ -4,23 +4,30 @@ import crc32 from 'buffer-crc32';
 export function sendFileToPrinter(printerIP: string, printerPort: number, fileBuffer: Buffer, fileName: string): Promise<string> {
   const fileSize = fileBuffer.length;
   const client = new Socket();
+
   client.connect(printerPort, printerIP, () => {
     client.write(`~M28 ${fileSize} 0:/user/${fileName}\r\n`); // Prepare Print
+    
     let chunkCounter = 0;
+
     for (let i = 0; i < fileBuffer.length; i += 4080) {
       let chunk = fileBuffer.slice(i, i + 4080);
+      const crcValue = crc32.unsigned(chunk).toString(16);
+      
       const header = Buffer.alloc(16);
       header.writeUInt32BE(0x5a5aa5a5, 0);
       header.writeUInt32BE(chunkCounter++, 4);
-      header.writeUInt32BE(chunk.length, 8);
-      header.writeUInt32BE(crc32.unsigned(chunk), 12);
+      header.writeUInt32BE(4096, 8);
+      header.write(crcValue.padStart(8, '0'), 12, 'hex');
+
       chunk = Buffer.concat([header, chunk]);
       if (chunk.length < 4096) {
         chunk = Buffer.concat([chunk, Buffer.alloc(4096 - chunk.length)]);
       }
-      console.log(chunk.length);
+
       client.write(chunk);
     }
+
     client.write('~M29\r\n'); //Save File
     client.write(`~M23 0:/user/${fileName}\r\n`); // Print Start
   });
