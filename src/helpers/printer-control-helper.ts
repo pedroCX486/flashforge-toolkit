bd4e3e743bfc519e7d8cce9baaa8ebd6c76d48ff
pedrocx486 <pedrocx486@gmail.com>
2023-05-28 19:45:10 -0300

    Add more GCodes

--- src/helpers/printer-control-helper.ts
+++ src/helpers/printer-control-helper.ts
@@ -1,5 +1,53 @@
 import { Socket } from "net";
 
+export const homePrinter = (printerAddr: string, printerPort: number) => {
+  const client = new Socket();
+  client.connect(printerPort, printerAddr, () => {
+    console.log('Homing...');
+    client.write('~G28\r\n'); // Move Home
+  });
+
+  return new Promise((resolve) => {
+    client.on('data', (data) => {
+      console.log(`Data returned:\n ${data.toString()}`);
+      client.destroy();
+      resolve(data.toString().includes('ok') ? "Homing Printer " : "Error Homing Printer");
+    });
+  });
+}
+
+export const resumePrint = (printerAddr: string, printerPort: number) => {
+  const client = new Socket();
+  client.connect(printerPort, printerAddr, () => {
+    console.log('Resuming print in progress...');
+    client.write('~M24\r\n'); // Resume
+  });
+
+  return new Promise((resolve) => {
+    client.on('data', (data) => {
+      console.log(`Data returned:\n ${data.toString()}`);
+      client.destroy();
+      resolve(data.toString().includes('ok') ? "Print Resumed" : "Error Resuming Print");
+    });
+  });
+}
+
+export const pausePrint = (printerAddr: string, printerPort: number) => {
+  const client = new Socket();
+  client.connect(printerPort, printerAddr, () => {
+    console.log('Pausing print in progress...');
+    client.write('~M25\r\n'); // Pause
+  });
+
+  return new Promise((resolve) => {
+    client.on('data', (data) => {
+      console.log(`Data returned:\n ${data.toString()}`);
+      client.destroy();
+      resolve(data.toString().includes('ok') ? "Print Paused" : "Error Pausing Print");
+    });
+  });
+}
+
 export const stopPrint = (printerAddr: string, printerPort: number) => {
   const client = new Socket();
   client.connect(printerPort, printerAddr, () => {
