a203cf8cf2f7f14c3808860d910e3fa2071a9c8e
pedrocx486 <pedrocx486@gmail.com>
2023-05-28 07:39:24 -0300

    Fix filename and add dynamic chunk length

--- src/helpers/printer-file-sender-helper.ts
+++ src/helpers/printer-file-sender-helper.ts
@@ -6,18 +6,18 @@ export function sendFileToPrinter(printerIP: string, printerPort: number, fileBu
   const client = new Socket();
 
   client.connect(printerPort, printerIP, () => {
-    client.write(`~M28 ${fileSize} 0:/user/${fileName}\r\n`); // Prepare Print
+    client.write(`~M28 ${fileSize} 0:/user/${fileName.replace(/ /g, '_')}\r\n`); // Prepare Print
     
     let chunkCounter = 0;
 
     for (let i = 0; i < fileBuffer.length; i += 4080) {
       let chunk = fileBuffer.slice(i, i + 4080);
       const crcValue = crc32.unsigned(chunk).toString(16);
-      
+
       const header = Buffer.alloc(16);
       header.writeUInt32BE(0x5a5aa5a5, 0);
       header.writeUInt32BE(chunkCounter++, 4);
-      header.writeUInt32BE(4096, 8);
+      header.writeUInt32BE(chunk.length, 8);
       header.write(crcValue.padStart(8, '0'), 12, 'hex');
 
       chunk = Buffer.concat([header, chunk]);
